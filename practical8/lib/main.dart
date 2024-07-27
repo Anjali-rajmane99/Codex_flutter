6a820ee4e05e016e806fdc778e653302c94d80f0
anjali <rajmaneanjali9@gmail.com>
2024-07-27 21:16:57 +0530

    practical9

--- practical8/lib/main.dart
+++ practical8/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Page1());
+    return MaterialApp(home: Screen2());
   }
 }
 
